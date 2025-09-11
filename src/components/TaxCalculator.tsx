72e80c3559392fa090d6ad412b3f2f739100101e
tm-jstephan <92997697+tm-jstephan@users.noreply.github.com>
2025-09-10 20:30:22 -0400

    ajout du logo et ajustement du bloc avec logo

--- src/components/TaxCalculator.tsx
+++ src/components/TaxCalculator.tsx
@@ -97,11 +97,14 @@ const TaxCalculator: React.FC = () => {
 
 
       <div className="text-center mb-8 p-6 bg-main-blue rounded-xl">
-        <div className="flex items-center justify-center mb-4 flex-col">
+        <div className="flex items-center justify-center mb-4 header">
           <img src="public/icons/taxesqc-logo.svg" className="logo" />
-          <h1 className="text-3xl font-bold text-white">{t.title}</h1>
+          <div>
+            <h1 className="text-3xl font-bold text-white">{t.title}</h1>
+            <p className="text-white sub-header">{t.subtitle}</p>
+          </div>
         </div>
-        <p className="text-white">{t.subtitle}</p>
+        
       </div>
 
       <div className="grid lg:grid-cols-2 gap-8">
