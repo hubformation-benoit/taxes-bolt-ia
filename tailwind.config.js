a0671afcda0952ce8a06cd44e529f2cb6098e53d
Stéphane Franic <sfranic@gmail.com>
2025-09-19 00:29:34 -0400

    Ajuster le bleu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          '50': '#97c8f9',
+          '200': '#65abf5',
+          '600': '#213e84',
+          '700': '#213e84',
+          '900': '#213e84',
+        },
+      },
+    },
   },
   plugins: [],
 };
